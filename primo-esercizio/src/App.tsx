8a46a49aba14414c526dafe734fb5c2444c65a37
Nunzia Lampitelli <nunzia.lampitelli@outlook.com>
2024-09-03 13:22:51 +0200

    initial commit

---
+++ primo-esercizio/src/App.tsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+import './App.css'
+import Home from './home';
+import About from './about';
+import Contact from './contact';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
+const App = () => {
+	return (
+		<Router>
+			<div>
+				<nav>
+					<ul>
+						<li>
+							<Link to="/">Home</Link>
+						</li>
+						<li>
+							<Link to="/about">About</Link>
+						</li>
+						<li>
+							<Link to="/contact">Contact</Link>
+						</li>
+					</ul>
+				</nav>
+
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/about" element={<About />} />
+					<Route path="/contact" element={<Contact />} />
+				</Routes>
+			</div>
+		</Router>
+	);
+};
+
+export default App;
---
+++ secondo-esercizio/src/components/settings.tsx
@@ -0,0 +1,2 @@
+import React from 'react';
+import {Outlet, Link} from 'react-router-dom;'
